bde673fd22f8f1333d749c03991916bfbf5c3b03
Adam Tyson <code@adamltyson.com>
2023-01-03 11:44:41 +0000

    Update usage (#1)
    * generalise
    
    * improve output

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# bg-stats
+# org-stats
+
+### Usage:
+
+```bash
+org_stats ORG_NAME GITHUB_TOKEN
+```
+e.g.:
+```bash
+org_stats brainglobe ghp_aaabbbcccdddeeefffggghhh
+```
+
+Output:
+```
+Total stars: 799, Total contributors: 54, Monthly downloads: 10204, Total
+downloads: 1446429
+```
--- org_stats/main.py
+++ org_stats/main.py
@@ -3,13 +3,22 @@ import typer
 from org_stats.stats import github_stats, pepy_stats, pypi_stats
 
 
-def main(github_token: str, organisation: str = "brainglobe"):
+def main(organisation: str, github_token: str):
     repos, stars, contributors = github_stats.run(github_token, organisation)
     pypi_monthly_downloads = pypi_stats.run(repos)
     pepy_overall_downloads = pepy_stats.run(repos)
 
-    print(stars, contributors, pypi_monthly_downloads, pepy_overall_downloads)
+    print(
+        f"Total stars: {stars}, "
+        f"Total contributors: {contributors}, "
+        f"Monthly downloads: {pypi_monthly_downloads}, "
+        f"Total downloads: {pepy_overall_downloads}"
+    )
+
+
+def run():
+    typer.run(main)
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    run()
